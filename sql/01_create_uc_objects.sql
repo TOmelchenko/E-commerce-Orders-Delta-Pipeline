c5ba4afe46a00ce383efb244658f9f6397078f27
t.omel778@gmail.com <t.omel778@gmail.com>
2026-04-22 10:54:17 +0000

    updated sql/01_create_uc_objects.sql

---
+++ sql/01_create_uc_objects.sql
@@ -0,0 +1,6 @@
+CREATE CATALOG IF NOT EXISTS retail_dev;
+CREATE SCHEMA IF NOT EXISTS retail_dev.orders;
+CREATE VOLUME IF NOT EXISTS retail_dev.orders.landing;
+CREATE VOLUME IF NOT EXISTS retail_dev.orders.checkpoints;
+
+
